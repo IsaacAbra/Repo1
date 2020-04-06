5350b5549764d43481bfaa2d8f92290c43d0b557
IsaacAbra <56645296+IsaacAbra@users.noreply.github.com>
2020-04-06 14:13:58 -0400

    Add files via upload

---
+++ memory-game/js/main.js
@@ -0,0 +1,76 @@
+console.log("Up and running!");
+
+let cardOne = "king";
+let cardTwo = "king";
+let cardThree = "queen";
+let cardFour = "queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
+
+
+var cards = [];
+var cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+function checkForMatch();
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+};
+checkForMatch();
+
+function flipCard(document.getAttribute(data-id));
+console.log(cards[cardId].cardImage)
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+console.log("Users flipped" + "cards[cardId].rank");
+cardid();
+console.log("cards[cardId].cardImage");
+console.log("cards[cardId].suit");
+
+cardsInPlay.push(cards[cardId].rank)
+
+const cards = [
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: "images/queen-of-hearts.png",
+},
+{
+rank: 'queen',
+suits: 'diamonds',
+cardImage: "images/queen-of-diamonds.png"
+}
+{
+rank: 'king',
+suits: 'hearts',
+cardImage: "images/king-of-hearts.png"
+}
+rank: 'king',
+suits: 'diamonds',
+cardImage: "images/king-of-diamonds.png"
+];
+
+var createBoard = function () { 
+	for (let i = 0; i < cards.length; i++){
+		let king-of-diamonds = document.createElement('images/back.png');
+		let king-of-hearts = document.createElement('images/back.png');
+		let queen-of-diamonds = document.createElement('images/back.png');
+		let queen-of-hearts = document.createElement('images/back.png');
+		 cardElement.addEventListener()('data-id', 'attributeValue');
+		 appendChild(cardElement);
+		 cardElement.flipCard('attributeName', 'attributeValue');
+	}
+}
+
+createBoard();
